b096dc6ee96be34026ba8391d87fb43b5e3952c5
bhavesh-gulabani <bhaveshgulabani1@gmail.com>
2024-07-09 15:44:29 +0530

    feat: add user organizations

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,37 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+
+const isPublicRoute = createRouteMatcher(['/']);
 
 const isProtectedRoute = createRouteMatcher([
-  // TODO: Add protected routes here
-  // '/protected(.*)',
+  '/select-org(.*)',
+  '/organization(.*)',
 ]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+  if (isProtectedRoute(req)) {
+    auth().protect();
+  }
+
+  const { userId, orgId } = auth();
+
+  if (userId && isPublicRoute(req)) {
+    let path = '/select-org';
+
+    if (orgId) {
+      path = `/organization/${orgId}`;
+    }
+
+    const orgSelection = new URL(path, req.url);
+
+    return NextResponse.redirect(orgSelection);
+  }
+
+  if (userId && !orgId && req.nextUrl.pathname !== '/select-org') {
+    const orgSelection = new URL('/select-org', req.url);
+
+    return NextResponse.redirect(orgSelection);
+  }
 });
 
 export const config = {
